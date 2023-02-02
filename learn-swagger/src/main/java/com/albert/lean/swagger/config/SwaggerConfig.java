3648982c1551cad4284e6c2ac4d8f360122d57ba
albert <albert.cui.hao@gmail.com>
2023-02-02 21:46:56 +0800

    token

--- learn-swagger/src/main/java/com/albert/lean/swagger/config/SwaggerConfig.java
+++ learn-swagger/src/main/java/com/albert/lean/swagger/config/SwaggerConfig.java
@@ -1,10 +1,19 @@
 package com.albert.lean.swagger.config;
 
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author Albert
@@ -13,20 +22,27 @@ import org.springframework.context.annotation.Configuration;
 public class SwaggerConfig {
 
     @Value("${albert.swagger.token_header}")
-    private String TOKEN_HEADER;
-    @Value("${albert.swagger.base_package}")
-    private String BASE_PACKAGE;
-
-    @Bean
-    public OpenAPI createOpenAPI() {
-        return new OpenAPI().info(apiInfo());
-    }
+    private String tokenHeader;
 
     //swagger-ui/index.html
-    public Info apiInfo() {
-        return new Info()
-                .title("SpringDoc API Test")
-                .description("SpringDoc Simple Application Test")
-                .version("0.0.1");
+    @Bean
+    public OpenAPI createOpenAPI() {
+        OpenAPI openAPI = new OpenAPI();
+        //添加header
+        Map<String, SecurityScheme> map = new HashMap<>();
+        map.put(tokenHeader, new SecurityScheme().type(SecurityScheme.Type.APIKEY).in(SecurityScheme.In.HEADER).name(tokenHeader));
+        openAPI.components(new Components().securitySchemes(map));
+        map.keySet().forEach(key -> openAPI.addSecurityItem(new SecurityRequirement().addList(key)));
+        openAPI.info(new Info().title("SpringShop API")
+                        .description("Spring shop sample application")
+                        .version("0.0.1")
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("http://springdoc.org")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("SpringShop Wiki Documentation")
+                        .url("https://springshop.wiki.github.org/docs"));
+        return openAPI;
+
     }
 }
--- learn-swagger/src/main/java/com/albert/lean/swagger/controller/IndexController.java
+++ learn-swagger/src/main/java/com/albert/lean/swagger/controller/IndexController.java
@@ -3,6 +3,7 @@ package com.albert.lean.swagger.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,6 +14,7 @@ import java.util.StringJoiner;
 /**
  * @author Albert
  */
+@Tag(name = "IndexController")
 @RestController
 @RequestMapping("/index")
 public class IndexController {
