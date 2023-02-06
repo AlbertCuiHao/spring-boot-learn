7176b5f83de7bbf75075f2f2c02455c88bc4c41b
albert <albert.cui.hao@gmail.com>
2023-02-06 17:35:52 +0800

    spring datasource

--- 04_spring_datasource/src/main/java/com/albert/datasource/App.java
+++ 04_spring_datasource/src/main/java/com/albert/datasource/App.java
@@ -1,17 +1,17 @@
 package com.albert.datasource;
 
+import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.jdbc.core.JdbcTemplate;
-
-import javax.sql.DataSource;
 
 public class App {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
         //JdbcTemplate jdbcTemplate = (JdbcTemplate) context.getBean("jdbcTemplate");
-        DataSource dataSource = (DataSource) context.getBean("dataSource");
-        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+        HikariDataSource hikariDataSource = context.getBean("dataSource", HikariDataSource.class);
+        System.out.println(hikariDataSource);
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(hikariDataSource);
         System.out.println(jdbcTemplate);
     }
 }
