4ad8fb0f20a88a96ed6270d700cfb45fe0adbad5
albert <albert.cui.hao@gmail.com>
2023-02-07 21:11:30 +0800

    transaction

--- 07_spring_transaction/src/main/java/com/albert/transaction/service/BankService.java
+++ 07_spring_transaction/src/main/java/com/albert/transaction/service/BankService.java
@@ -1,8 +1,11 @@
 package com.albert.transaction.service;
 
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.sql.SQLException;
+
 public interface BankService {
-    @Transactional
+    @Transactional(propagation = Propagation.REQUIRED, rollbackFor = {SQLException.class, ArithmeticException.class})
     void transfer();
 }
