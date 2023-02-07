405cb62d103ead5a573931b07e819e179b794134
albert <albert.cui.hao@gmail.com>
2023-02-07 18:01:44 +0800

    datasource

--- 04_spring_datasource/src/main/java/com/albert/datasource/App.java
+++ 04_spring_datasource/src/main/java/com/albert/datasource/App.java
@@ -1,17 +1,22 @@
 package com.albert.datasource;
 
-import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.jdbc.core.JdbcTemplate;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class App {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        //JdbcTemplate jdbcTemplate = (JdbcTemplate) context.getBean("jdbcTemplate");
-        HikariDataSource hikariDataSource = context.getBean("dataSource", HikariDataSource.class);
-        System.out.println(hikariDataSource);
-        JdbcTemplate jdbcTemplate = new JdbcTemplate(hikariDataSource);
-        System.out.println(jdbcTemplate);
+        JdbcTemplate jdbcTemplate = context.getBean("jdbcTemplate", JdbcTemplate.class);
+        Connection connection = null;
+        try {
+            connection = jdbcTemplate.getDataSource().getConnection();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println(connection);
     }
 }
