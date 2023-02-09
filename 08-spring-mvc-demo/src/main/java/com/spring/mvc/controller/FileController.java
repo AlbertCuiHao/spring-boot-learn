086f711b966111bf54996d1a00e3ff445cda868f
albert <albert.cui.hao@gmail.com>
2023-02-09 21:11:24 +0800

    文件上传

--- 08-spring-mvc-demo/src/main/java/com/spring/mvc/controller/FileController.java
+++ 08-spring-mvc-demo/src/main/java/com/spring/mvc/controller/FileController.java
@@ -5,7 +5,11 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
@@ -36,5 +40,14 @@ public class FileController {
                 throw new RuntimeException(e);
             }
         }
+    }
+
+    @PostMapping("/upload")
+    @ResponseBody
+    public String upload(MultipartHttpServletRequest request) {
+        MultipartFile file = request.getFile("file");
+        String originalFilename = file.getOriginalFilename();
+        System.out.println(originalFilename);
+        return originalFilename;
     }
 }
