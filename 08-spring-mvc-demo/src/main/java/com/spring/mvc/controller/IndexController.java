9cc5d9641f8325809059cc57ccd10c04c4dddbc6
albert <albert.cui.hao@gmail.com>
2023-02-09 15:18:39 +0800

    JSON

--- 08-spring-mvc-demo/src/main/java/com/spring/mvc/controller/IndexController.java
+++ 08-spring-mvc-demo/src/main/java/com/spring/mvc/controller/IndexController.java
@@ -1,5 +1,6 @@
 package com.spring.mvc.controller;
 
+import com.spring.mvc.entity.UserEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -35,6 +36,15 @@ public class IndexController {
     public String requestParam(@RequestParam("requestParam") String requestParam) {
         System.out.println(requestParam);
         return requestParam;
+    }
+
+    @GetMapping("/response-body")
+    @ResponseBody
+    public UserEntity ResponseBody() {
+        UserEntity entity = new UserEntity();
+        entity.setName("张三");
+        entity.setAge(15);
+        return entity;
     }
 
 }
