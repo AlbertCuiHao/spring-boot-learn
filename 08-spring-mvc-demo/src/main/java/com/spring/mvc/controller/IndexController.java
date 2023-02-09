4546a15672294c29ab972a4ff4795ac8aa94de51
albert <albert.cui.hao@gmail.com>
2023-02-09 14:46:25 +0800

    中文乱码

--- 08-spring-mvc-demo/src/main/java/com/spring/mvc/controller/IndexController.java
+++ 08-spring-mvc-demo/src/main/java/com/spring/mvc/controller/IndexController.java
@@ -1,15 +1,40 @@
 package com.spring.mvc.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
+@RequestMapping("/index")
 public class IndexController {
-    @RequestMapping("/hello")
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
     @ResponseBody
     public String index() {
         return "hello";
+    }
+
+    @RequestMapping(value = "/params", method = RequestMethod.GET)
+    @ResponseBody
+    public String params() {
+        return "success";
+    }
+
+    @RequestMapping(value = "/pathvariable/{a}/{b}", method = RequestMethod.GET)
+    @ResponseBody
+    public String pathvariable(@PathVariable("a") String id, @PathVariable("b") String username) {
+        return id + username;
+    }
+
+    @GetMapping("/request-header")
+    @ResponseBody
+    public String requestHeader(@RequestHeader(name = "header") String header) {
+        return header;
+    }
+
+    @GetMapping("/request-param")
+    @ResponseBody
+    public String requestParam(@RequestParam("requestParam") String requestParam) {
+        System.out.println(requestParam);
+        return requestParam;
     }
 
 }
