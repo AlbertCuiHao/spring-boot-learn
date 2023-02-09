0a9667e37c31cfb68303b12f2b7b6b9a3734913d
albert <albert.cui.hao@gmail.com>
2023-02-09 16:25:45 +0800

    文件下载

---
+++ 08-spring-mvc-demo/src/main/java/com/spring/mvc/controller/FileController.java
@@ -0,0 +1,40 @@
+package com.spring.mvc.controller;
+
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
+@Controller
+@RequestMapping("/file")
+public class FileController {
+    @GetMapping("/download")
+    public void download(HttpSession session, HttpServletResponse response) {
+        ServletContext servletContext = session.getServletContext();
+        String realPath = servletContext.getRealPath(File.separator + "static" + File.separator + "1.png");
+        System.out.println(realPath);
+        File file = new File(realPath);
+        if (file.exists()) {
+            String filename = file.getName();
+            try (InputStream inputStream = new BufferedInputStream(new FileInputStream(file)); OutputStream outputStream = new BufferedOutputStream(response.getOutputStream())) {
+
+                response.setContentType("application/octet-stream;charset=UTF-8");
+                response.addHeader("Content-Disposition", "attachment;filename=" + new String(filename.getBytes(), StandardCharsets.ISO_8859_1));
+                response.setContentType("application/octet-stream");
+
+                byte[] buffer = new byte[1024];
+                int count = -1;
+                while ((count = inputStream.read(buffer)) != -1) {
+                    outputStream.write(buffer, 0, count);
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
