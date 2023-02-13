f7a23082a937e107bb9fd153c14dc9d5cd1e9087
albert <albert.cui.hao@gmail.com>
2023-02-13 14:37:07 +0800

    拦截器

--- 08-spring-mvc-demo/src/main/java/com/spring/mvc/interceptor/FirstHandlerInterceptor.java
+++ 08-spring-mvc-demo/src/main/java/com/spring/mvc/interceptor/FirstHandlerInterceptor.java
@@ -6,18 +6,21 @@ import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 public class FirstHandlerInterceptor implements HandlerInterceptor {
+    //方法执行前
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         System.out.println("preHandle");
         return HandlerInterceptor.super.preHandle(request, response, handler);
     }
 
+    //方法执行后
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
         System.out.println("postHandle");
         HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
     }
 
+    //处理完视图和模型数据,渲染视图完毕之后
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
         System.out.println("afterCompletion");
