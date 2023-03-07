d87847d5ff59c4aa579baf1c43871c3f3161d96b
albert <albert.cui.hao@gmail.com>
2023-03-07 12:45:15 +0800

    transaction

--- 07-spring-transaction/src/main/java/com/albert/transaction/service/BankService.java
+++ 07-spring-transaction/src/main/java/com/albert/transaction/service/BankService.java
@@ -6,6 +6,13 @@ import org.springframework.transaction.annotation.Transactional;
 import java.sql.SQLException;
 
 public interface BankService {
+    /*    REQUIRED：默认值，支持当前事务，如果没有事务会创建一个新的事务
+        SUPPORTS：支持当前事务，如果没有事务的话以非事务方式执行
+        MANDATORY：支持当前事务，如果没有事务抛出异常
+        REQUIRES_NEW：创建一个新的事务并挂起当前事务
+        NOT_SUPPORTED：以非事务方式执行，如果当前存在事务则将当前事务挂起
+        NEVER：以非事务方式进行，如果存在事务则抛出异常
+        NESTED：如果当前存在事务，则在嵌套事务内执行。如果当前没有事务，则进行与REQUIRED类似的操作*/
     @Transactional(propagation = Propagation.REQUIRED, rollbackFor = {SQLException.class, ArithmeticException.class})
     void transfer();
 }
--- 07-spring-transaction/src/main/java/com/albert/transaction/service/impl/BankServiceImpl.java
+++ 07-spring-transaction/src/main/java/com/albert/transaction/service/impl/BankServiceImpl.java
@@ -4,8 +4,6 @@ import com.albert.transaction.dao.BankDao;
 import com.albert.transaction.service.BankService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class BankServiceImpl implements BankService {
@@ -16,14 +14,6 @@ public class BankServiceImpl implements BankService {
         this.bankDao = bankDao;
     }
 
-//        REQUIRED：默认值，支持当前事务，如果没有事务会创建一个新的事务
-//        SUPPORTS：支持当前事务，如果没有事务的话以非事务方式执行
-//        MANDATORY：支持当前事务，如果没有事务抛出异常
-//        REQUIRES_NEW：创建一个新的事务并挂起当前事务
-//        NOT_SUPPORTED：以非事务方式执行，如果当前存在事务则将当前事务挂起
-//        NEVER：以非事务方式进行，如果存在事务则抛出异常
-//        NESTED：如果当前存在事务，则在嵌套事务内执行。如果当前没有事务，则进行与REQUIRED类似的操作
-    @Transactional(propagation = Propagation.REQUIRED)
     @Override
     public void transfer() {
         bankDao.in("B", 50);
