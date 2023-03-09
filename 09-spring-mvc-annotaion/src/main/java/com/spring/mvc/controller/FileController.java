48e5416ee82694fd62d7259bf33907a4d313cf1a
albert <albert.cui.hao@gmail.com>
2023-03-09 08:55:50 +0800

    file upload

--- 09-spring-mvc-annotaion/src/main/java/com/spring/mvc/controller/FileController.java
+++ 09-spring-mvc-annotaion/src/main/java/com/spring/mvc/controller/FileController.java
@@ -1,18 +1,12 @@
 package com.spring.mvc.controller;
 
-import jakarta.servlet.ServletContext;
-import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
-
-import java.io.*;
-import java.nio.charset.StandardCharsets;
 
 @Controller
 @RequestMapping("/file")
@@ -24,5 +18,14 @@ public class FileController {
         String originalFilename = file.getOriginalFilename();
         System.out.println(originalFilename);
         return originalFilename;
+    }
+
+    @PostMapping("/importData")
+    @ResponseBody
+    public String importData(@RequestParam("file") MultipartFile file) {
+        if (!file.isEmpty()) {
+            System.out.println(file.getOriginalFilename());
+        }
+        return "123";
     }
 }
